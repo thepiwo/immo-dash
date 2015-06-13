544cb70703fc7f607574b8123317f7695158d113
Philipp Piwowarsky <philipp@piwowarsky.de>
2015-06-13 15:54:49 +0200

    fix

--- app/controllers/ImmobilienController.java
+++ app/controllers/ImmobilienController.java
@@ -16,13 +16,13 @@ public class ImmobilienController extends Controller {
         return redirect(routes.ImmobilienController.index(immo.getId()));
     }
 
-    public Result index(int id){
-        Immobilie immo = Immobilie.find.where().eq("id",id).findUnique();
-        if(immo== null)return  ok(createImmo.render(Immobilie.immoForm));
+    public Result index(int id) {
+        Immobilie immo = Immobilie.find.where().eq("id", id).findUnique();
+        if (immo == null) {
+            return redirect(routes.ImmobilienController.create());
+        }
         return ok(showImmo.render(immo));
     }
-
-
 
 
 }
