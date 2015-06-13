77146cab98ac438fe4f784aac34b24ef8b6add90
falk <burdahackday@hda1.de>
2015-06-13 15:04:05 +0200

    immo controller

--- app/controllers/ImmobilienController.java
+++ app/controllers/ImmobilienController.java
@@ -4,16 +4,18 @@ import models.Immobilie;
 import play.mvc.Controller;
 import play.mvc.Result;
 
-/**
- * Created by fa on 13.06.15.
- */
+import scala.xml.Null;
+import views.html.createImmo;
+
 public class ImmobilienController extends Controller {
 
     public Result index(int id){
         Immobilie immo = Immobilie.find.where().eq("id",id).findUnique();
-        if(immo== null)return  badRequest("badRequest");
+        if(immo== null)return  ok(createImmo.render());
         return TODO;
     }
+
+
 
 
 }
