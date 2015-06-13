cc6087f8fe16a8a7608751a6cf2f25a7108bc5d7
Rupert <rupert.zinnecker@gmail.com>
2015-06-13 12:12:45 +0200

    neuen link zu test.scala.html eingebaut

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -7,6 +7,10 @@ import views.html.*;
 
 public class Application extends Controller {
 
+    public Result getTest() {
+        return ok(test.render("blub"));
+    }
+
     public Result index() {
         return ok(index.render("Your new application is ready."));
     }
